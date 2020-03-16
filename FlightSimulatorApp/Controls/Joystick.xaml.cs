01c0ab98e2aca7f8ab825d56db8dda102ac78eb4
Gilad Bet Yaakov <gilad.betyaakov@live.biu.ac.il>
2020-03-16 19:48:57 +0200

    knobs

---
+++ FlightSimulatorApp/Controls/Joystick.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Shapes;
+
+namespace FlightSimulatorApp.Controls
+{
+    public partial class Joystick : UserControl
+    {
+        double mouseX, mouseY;
+        List<Ellipse> knobs;
+        bool mousePressed;
+        public Joystick()
+        {
+            InitializeComponent();
+            mousePressed = false;
+            knobs = new List<Ellipse> { KnobMask, KnobBase5, KnobBase4, KnobBase3, KnobBase2, KnobBase1 };
+        }
+
+        private setKnobPos
+
+        private void centerKnob_Completed(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KnobMask_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void KnobMask_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            mousePressed = false;
+        }
+
+        private void KnobMask_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!mousePressed)
+            {
+                mouseX = e.GetPosition(sender as Ellipse).X;
+                mouseY = e.GetPosition(sender as Ellipse).Y;
+                Console.WriteLine(mouseX);
+            }
+        }
+    }
+}
